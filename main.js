8ddb17bfe0519e8071bafa2297939a2def499487
Bo Yuan <byuan@ama.com.au>
2016-02-17 10:32:05 +1100

    angular auto validate

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
-var app = angular.module('minmax', []);
+var app = angular.module('minmax', ['jcs-autoValidate']);
 
-app.controller('MinMaxCtrl', function($scope, $http) {
-    
+app.run(function(defaultErrorMessageResolver) {
+    defaultErrorMessageResolver.getErrorMessages().then(function(errorMessages) {
+        errorMessages['tooYoung'] = 'You must be at least {0} years old to use this site';
+        errorMessages['tooOld'] = 'You must be max {0} years old to use this site';
+        errorMessages['badUsername'] = 'Username can only contain numbers and letters and _';
+    });
+});
+
+app.controller('MinMaxCtrl', function ($scope, $http) {
+
     $scope.formModel = {};
-    
-    $scope.onSubmit = function(valid) {
-        
-        if (valid) {
-            console.log("Hey I'm submitted");
-            console.log($scope.formModel);
 
-            $http.post('http://restcms.local/api/v1/auth/register', $scope.formModel)
-                    .success(function(data) {
-                        console.log(":) ", data);
-                    }).error(function(err) {
-                        console.log("Register error: " + err);
-                    });
-        } else {
-            console.log("Invalid form");
-        }
+    $scope.onSubmit = function () {
+
+
+        console.log("Hey I'm submitted");
+        console.log($scope.formModel);
+
+        $http.post('http://restcms.local/api/v1/auth/register', $scope.formModel)
+                .success(function (data) {
+                    console.log(":) ", data);
+                }).error(function (err) {
+            console.log("Register error: " + err);
+        });
+
     };
-    
+
 });
